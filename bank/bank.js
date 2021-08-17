93783a3c312b85e4bb3c27d5a3e36afdd03092a0
Shaown29 <shaownabir@outlook.com>
2021-08-17 21:36:28 +0600

    seventh-commit

--- bank/bank.js
+++ bank/bank.js
@@ -6,10 +6,11 @@ document.getElementById('login-btn').addEventListener('click',function(){
    const password=passwordField.value;
    if(email=='abcdef@bank.com' && password==12345 ){
        console.log('valid user');
+       window.location.href='banking.html';
    }
    else{
        console.log('not valid');
    }
-   window.location.href='banking.html';
+   
 
 })
